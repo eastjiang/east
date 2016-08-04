63c743028c95183276151e3b62d73e589387b656
TMD <jiangdongeast0925@outlook.com>
2016-08-04 21:43:02 +0800

    个人练习APP -- 汽车之家APP

--- app/src/main/java/com/eastproject/app/LoginActivity.java
+++ app/src/main/java/com/eastproject/app/LoginActivity.java
@@ -1,12 +1,9 @@
 package com.eastproject.app;
 
-import android.graphics.Bitmap;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import com.eastproject.app.utils.BitmapUtil;
 
 /**
  * 登录页面
@@ -17,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
