056cadf3ba0bf45b38744950f34d222cb8c7a69c
TMD <jiangdongeast0925@outlook.com>
2016-08-04 21:47:41 +0800

    test

--- app/src/main/java/com/eastproject/app/LoginActivity.java
+++ app/src/main/java/com/eastproject/app/LoginActivity.java
@@ -14,7 +14,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
 }
